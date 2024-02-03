fb8251c5cdf6ac48acc775a14c7ad0f280a0506d
Magdalena Idzikowska <midzikowska@students.nic.edu>
2024-02-03 13:00:49 -0800

    Lost fight with grid
    I still have to improve Profile page but now im moving on to Create New Duckpage

--- profile.php
+++ profile.php
@@ -12,28 +12,33 @@ include('./asset/components/head.php');
     include('./asset/components/navigation.php');
     ?>
     <main>
-        <div class="content">   
-        <h1>DUCK PROFILE</h1>
-        <h2>Meet Quackwell Frostington!</h2>
-        <p>Get to know our duck models and see their cool styles. Each duck has its own look and personality. Find some inspiration for your own ducks!</p>
-        <h2>Biography</h2>
-        <p>Born in the heart of the frosty pond, Duckington Frostwell emerged into the world with a penchant for chilly elegance. </p>
+        <div class="page-title">
+            <h1>DUCK PROFILE</h1>
+            <!-- <h2>Meet Quackwell Frostington!</h2> -->
+            <!-- <p>Get to know our duck models and see their cool styles. Each duck has its own look and personality. Find some inspiration for your own ducks!</p> -->
+            <!-- <h2>Biography</h2> -->
+            <!-- <p>Born in the heart of the frosty pond, Duckington Frostwell emerged into the world with a penchant for chilly elegance. </p>
         <p>From the earliest days, this dapper duck showcased a flair for winter fashion, donning his first Victorian-style coat at a tender age.</p>
         <p>Duckington's frosty charisma and love for winter wonderlands quickly made him a style icon, setting the stage for a biography filled with cool adventures and quack-tastic charm.</p>
-        </div>
-        
-        <div class="gallery-profile">
-            <div class="grid-item-profile">
-                <img src="./asset/images/Duck-Coatk - Redbubble 2024-01-31 14-19-59.png" alt="Duck wearing coat">
-                <div class="grid-profile-content">
-                    <h2>Duckington Frostwell</h2>
-                    <h3>Shop Signature Style</h3>
-                    <p>Quackwell is always wrapped up in his favorite Victorian-style coat, ensuring he's both cozy and stylish. With a blend of timeless charm and feather-friendly warmth, this frosty fellow sets the trend for winter chic.</p>
+        </div> -->
+            <div class="wrapper">
+                <div class="grid-container">
+                    <div class="grid-profile-photo">
+                        <img src="./asset/images/Duck-Coatk - Redbubble 2024-01-31 14-19-59.png" alt="Duck wearing coat">
+                        <div class="grid-profile-content">
+                            <h2>Duckington Frostwell</h2>
+                            <p><b>Fav Food:</b> Sourdough Breadcrumbs</p>
+                            <h3>Biography</h3>
+                            <!-- <p>Quackwell is always wrapped up in his favorite Victorian-style coat, ensuring he's both cozy and stylish. With a blend of timeless charm and feather-friendly warmth, this frosty fellow sets the trend for winter chic.</p> -->
+                            <p>Born in the heart of the frosty pond, Duckington Frostwell emerged into the world with a penchant for chilly elegance. </p>
+                            <p>From the earliest days, this dapper duck showcased a flair for winter fashion, donning his first Victorian-style coat at a tender age.</p>
+                            <p>Duckington's frosty charisma and love for winter wonderlands quickly made him a style icon, setting the stage for a biography filled with cool adventures and quack-tastic charm.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-    
-            </div>
         </div>
+      
     </main>
     <?php
     include('./asset/components/footer.php');
