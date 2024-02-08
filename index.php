1b0ef547d929b5163d2b5ad6e60ffd081d1f12aa
Magdalena Idzikowska <midzikowska@students.nic.edu>
2024-02-08 08:21:41 -0800

    Submission for Assignment Assignment 5.1: Create a Duck Directory (Part 2)
    This part was achieved on Monday (February 5th) where we were adding  php code to home page the way is taking information from Database like : name , img , favorite food.
    
    However, today, I fix my problem with grid on Home Page :
     <h3>Favorite Foods</h3>
                            <?php foreach ($food_list as $food); ?>
                            <p><?php echo $food ?></p>
                        </div>
                    </div>
                <?php endforeach ?>
            </div>
    
    before i had <?php endforeach ?> after
      <p><?php echo $food ?></p>
    
    it turned out that this was breaking my grid layout on Monday.
    
    ******************************
    
    Wednesay (Feb 7th)update:
    adding php code to create-duck.php succesfully.

--- index.php
+++ index.php
@@ -34,64 +34,26 @@ include('./asset/components/head.php');
     <main>
         <h1>My Ducks</h1>
         <h2></h2>
-        <p>Check out our awesome collection of duck clothes. From snazzy bow ties to stylish hats, your ducks can look as cool as can be. </p>
+        <p>Check out our unique collection of duck clothes. From snazzy bow ties to stylish hats, your ducks can look as cool as can be. </p>
         <div class="gallery">
-
             <?php foreach ($ducks as $duck) :  ?>
-
                 <div class="grid-item">
-                    <img src="<?php echo $duck["img_src"]; ?>" alt="Duck Name 1">
-
-                    <!-- <h3>Favorite Things to Do</h3>
-                    <p>Strolling by the frosty pond, enjoying chilly swims.</p>
-                    <h3>Favorite Clothes</h3>
-                    <p>A stylish Victorian coat to stay warm and dapper.</p> -->
+                    <div class="grid-img">
+                        <img src="<?php echo $duck["img_src"]; ?>" alt="Duck Name 1">
+                    </div>
+                    <div class="grid-item-content">
+                        <H2><?php echo $duck["name"]; ?> </H2>
+                        <?php
+                        // Breaks duck food into array
+                        $food_list = explode(",", $duck["favorite_foods"]);
+                        ?>
+                        <h3>Favorite Foods</h3>
+                        <?php foreach ($food_list as $food); ?>
+                        <p><?php echo $food ?></p>
+                    </div>
                 </div>
-                <div class="grid-item-content">
-                    <H2><?php echo $duck["name"]; ?> </H2>
-                    <?php
-                    // Breaks duck food into array
-                    $food_list = explode(",", $duck["favorite_foods"]);
-                    ?>
-
-                    <!--  -->
-                    <h3>Favorite Foods</h3>
-                    <?php foreach ($food_list as $food); ?>
-                    <p><?php echo $food ?></p>
-                <?php endforeach ?>
-                </div>
-
-                <!-- <div class="grid-item">
-                <img src="./asset/images/duck-hat.jpeg" alt="Duck Name 2">
-                <div class="grid-item-content">
-                    <h2>Harper Quackley</h2>
-                    <h3>Favorite Foods</h3>
-                    <p>Worms</p>
-                    <p>Small Fish</p>
-                    <p>Amphibians</p>
-                    <h3>Favorite Things to Do</h3>
-                    <p>Tending to the duck farm, splashing in puddles after a good rain.</p>
-                    <h3>Favorite Clothes</h3>
-                    <p> A classic straw hat for shade while working on the farm.</p>
-                </div>
-            </div>
-            <div class="grid-item">
-                <img src="./asset/images/duck-she.jpeg" alt="Duck Name 3">
-                <div class="grid-item-content">
-                    <h2>Reginald Drizzleton</h2>
-                    <h3>Favorite Foods</h3>
-                    <p>Algae</p>
-                    <p>Small Fish</p>
-                    <p>Human-Provided Foods</p>
-                    <h3>Favorite Things to Do</h3>
-                    <p>Sipping tea by the English garden, strutting down the duckwalk.</p>
-                    <h3>Favorite Clothes</h3>
-                    <p> A chic ensemble with a stylish umbrella always at the ready for the inevitable English rain..</p>
-                </div>
-            </div> -->
-
+            <?php endforeach ?>
         </div>
-        <!-- </div> -->
     </main>
     <?php
     include('./asset/components/footer.php');
